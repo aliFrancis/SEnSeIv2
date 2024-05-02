e349e7e52d2571966781bd19a9ab83ba7b07474d
aliFrancis <alistair.m.francis@gmail.com>
2024-05-02 09:53:02 +0000

    Update for pip packaging

--- setup.py
+++ setup.py
@@ -3,8 +3,19 @@
 from setuptools import setup, find_packages
 import os
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+REQUIRED = [
+    "huggingface-hub >= 0.17",
+    "numpy",
+    "matplotlib >= 3.6.2",
+    "pyyaml >= 6.0",
+    "rasterio >= 1.3.4",
+    "scikit-image >= 0.19.3",
+    "segmentation-models-pytorch == 0.3.2",
+    "six",
+    "torchmetrics >= 1.1",
+    "transformers >= 4.34.0",
+    "wandb >= 0.15.12"
+]
 
 
 
@@ -15,20 +26,20 @@ def package_files(directory):
             paths.append(os.path.join('..', r, filename))
     return paths
 
-extra_files = package_files('hf_models')
+# extra_files = package_files('hf_models')
 
 
 setup(
     name='senseiv2',
-    packages=find_packages(),
-    package_data={'': extra_files},
+    packages=find_packages(exclude=['hf_models/*']),
+    # package_data={'': extra_files},
     entry_points={
         'console_scripts': [
             'senseiv2 = senseiv2.inference:main'
         ]
     },
-    version='0.1',
+    version='1.1',
     description='SEnSeIv2',
     author='Alistair Francis',
-    install_requires=required,
+    install_requires=REQUIRED,
 )
